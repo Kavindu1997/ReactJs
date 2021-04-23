3ce8bf13135a424a8c1c54005e672e3f37d55362
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-04-23 18:35:29 +0530

    my app modifications

--- my-app-app/src/index.js
+++ my-app-app/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,  useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -54,4 +54,83 @@ const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
 ReactDOM.render(
   <ContactManager data={contacts} />, 
   document.getElementById('root')
+);
+
+
+
+
+
+function AddForm() {
+  const [sum, setSum] = useState(0);
+  const [num, setNum] = useState(0);
+
+  function handleFormChange(e) {
+    setNum(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    setSum(sum + Number(num));
+    e.preventDefault();
+  }
+
+  return <form onSubmit={handleSubmit}>
+  <input type="number" value={num} onChange={handleFormChange} />
+  <input type="submit" value="Add" />
+  <p> Sum is {sum} </p>
+  </form>;
+}
+
+const el1 = <AddForm />; 
+ReactDOM.render(
+  el1, 
+  document.getElementById('cal')
+);
+
+
+
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  // useEffect(() => {
+  //   alert("Number of clicks: " + counter);
+  // });
+
+  function increment() {
+    setCounter(counter+1);
+  }
+  return <div>
+  <p>{counter}</p>
+  <button onClick={increment}>Increment</button>
+  </div>;
+}
+
+const el2 = <Counter />; 
+ReactDOM.render(
+  el2, 
+  document.getElementById('counter')
+);
+
+
+
+function Converter() {
+  const [km, setKm] = useState(0);
+
+  function handleChange(e) {
+    setKm(e.target.value);
+  }
+  function convert(km) {
+    return (km/1.609).toFixed(2);
+  }
+
+  return <div>
+  <input type="text" value={km} onChange={handleChange} />
+  <p> {km} km is {convert(km)} miles </p>
+  </div>;
+}
+
+const el3 = <Converter />; 
+ReactDOM.render(
+  el3, 
+  document.getElementById('converter')
 );
